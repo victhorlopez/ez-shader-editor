342297cbad221a0d696b2deeebc0154db9f7e90b
vik <victoret86@gmail.com>
2014-11-03 23:23:12 +0100

    testing perlin noise

--- shaders/noise/noise.fs
+++ shaders/noise/noise.fs
@@ -6,8 +6,7 @@ uniform float u_ambient;
 uniform vec3 u_eye;
 
 uniform sampler2D u_color_texture;
-uniform sampler2D u_normal_texture;
-uniform sampler2D u_specular_texture;
+
 
 varying vec3 v_normal;
 varying vec3 v_pos;
@@ -20,15 +19,14 @@ void main()
 
         normalize(v_normal);
         vec4 tex_color = texture2D(u_color_texture, v_coord);
-        vec4 normal = texture2D(u_normal_texture, v_coord );
-        vec4 specular = texture2D(u_specular_texture, v_coord );
+
 
         vec3 L = normalize(u_light_dir);
         float D =  max(0.0, dot( L , v_normal)); // diffuse component
         vec3 point_to_eye = normalize( u_eye - v_pos);
         vec3 H = reflect(-L,v_normal); // vector reflected in the incident point
         float E = pow(max(0.0, dot( H , point_to_eye)),8.0); // the closer the dot is to 1 the shiner
-        gl_FragColor = u_color * tex_color *  (u_ambient + D) + u_light_color * E * specular.x;
+        gl_FragColor = u_color *   (u_ambient + D ) + u_light_color * E;
 
 
 }
