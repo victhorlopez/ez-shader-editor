2e7f018b3836847199a4f39333331018c3056fa7
vik <victoret86@gmail.com>
2015-01-19 22:07:26 +0100

    ez.render updated

--- js/external/ez.render.js
+++ js/external/ez.render.js
@@ -302,6 +302,7 @@ EZ.CameraController = function ( renderer ) {
     this.ctx = renderer.context; // ctx = context
     this.cam = null;
     this.needs_update = true;
+    this.needs_rotation_update = true;
 
     this.radius = vec3.create();
 
@@ -315,18 +316,25 @@ EZ.CameraController = function ( renderer ) {
     this.onMouseMove = function (e) {
         if(e.dragging){
             var delta = e.deltax > 0.1 || e.deltax < -0.1 ? -e.deltax : 0;
-            if(delta)
+            if(delta){
                 quat.setAxisAngle( that.cam.quat, [0,1,0], delta * DEG2RAD );
-            else
-                quat.identity(that.cam.quat);
-            delta = e.deltay > 0.1 || e.deltay < -0.1 ? -e.deltay : 0;
-            if(delta)
-                quat.setAxisAngle( EZ.temp_quat, [1,0,0], delta * DEG2RAD );
-            else
-                quat.identity(EZ.temp_quat);
+                //quat.mul(that.cam.quat, that.cam.quat,EZ.temp_quat);
+                that.cam.needs_local_update = true;
+                that.needs_rot_update = true;
+                that.needs_update = true;
+            }
 
-            quat.mul(that.cam.quat,EZ.temp_quat, that.cam.quat);
-            that.needs_update = true;
+
+//            delta = e.deltay > 0.1 || e.deltay < -0.1 ? -e.deltay : 0;
+//            if(delta)
+//                quat.setAxisAngle( EZ.temp_quat, [1,0,0], delta * DEG2RAD );
+//            else
+//                quat.identity(EZ.temp_quat);
+//
+//
+
+
+
         }
     };
 
@@ -348,15 +356,15 @@ EZ.CameraController = function ( renderer ) {
             vec3.sub(EZ.temp_vec4,this.cam.position, this.cam.target);
             vec3.scale(EZ.temp_vec4, EZ.temp_vec4, this.scale);
 
-
-            //vec3.transformQuat(EZ.temp_vec4, EZ.temp_vec4,that.cam.quat );
-
+            if(this.needs_rot_update)
+                vec3.transformQuat(EZ.temp_vec4, EZ.temp_vec4,that.cam.quat );
             vec3.add(this.cam.position,this.cam.target, EZ.temp_vec4 );
 
-            quat.identity(that.cam.quat);
+
             this.scale = 1.0;
             this.cam.lookAt(this.cam.target);
             this.needs_update = false;
+            this.needs_rot_update = false
         }
     };
 
